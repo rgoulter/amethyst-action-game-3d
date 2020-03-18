6c84dcc1f9b97fa1b05ac5c07440fc562964ecf6
Richard <richard.goulter@gmail.com>
2020-03-18 21:31:48 +0700

    Simplify player.rs entity

--- src/player.rs
+++ src/player.rs
@@ -5,7 +5,6 @@ use amethyst::{
     core::{
         transform::Transform,
         Named,
-        Parent,
     },
     ecs::{NullStorage},
     ecs::prelude::{
@@ -30,43 +29,29 @@ pub fn init_player(
     assets: Assets,
     init_location: Transform
 ) -> Entity {
-    let player = world
-        .create_entity()
-        .with(init_location)
-        .with(Player)
-        .build();
-
-    let tank_transform = Transform::default();
     let mut tank_replace_material_targets: HashSet<Cow<'static, str>> =
         HashSet::new();
     tank_replace_material_targets.insert(Cow::Borrowed("TankBase"));
     tank_replace_material_targets.insert(Cow::Borrowed("Turret"));
     tank_replace_material_targets.insert(Cow::Borrowed("TurretGun"));
+
     let replace_material = ReplaceMaterial {
         targets: tank_replace_material_targets,
         replacement: Some(assets.tank_blue_material.clone()),
     };
-    let _model_rotation = world
-        .create_entity()
-        .with(Parent { entity: player })
-        .with(Named { name: Cow::Borrowed("player_tank_replace") })
-        .with(replace_material)
-        .with(tank_transform)
-        .build();
 
-    // front of the model in Blender is -Y;
-    // but the GLTF export has it as +Z.
     let tank_gltf_mesh = assets.tank_gltf.clone();
     {
         let w : &mut World = world;
         print_gltf_info(w, &tank_gltf_mesh);
     }
-    let _tank_entity = world
-        .create_entity()
-        .with(tank_gltf_mesh)
-        .with(Transform::default())
-        .with(Parent { entity: _model_rotation })
-        .build();
 
-    player
+    world
+        .create_entity()
+        .with(init_location)
+        .with(Named { name: Cow::Borrowed("player_tank_replace") })
+        .with(Player)
+        .with(replace_material)
+        .with(tank_gltf_mesh)
+        .build()
 }
