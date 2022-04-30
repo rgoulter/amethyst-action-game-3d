152a3b4558133452b25696969025541eaab3951a
Richard Goulter <richard.goulter@gmail.com>
2022-04-30 12:04:55 +0700

    add shell.nix with nixpkgs pinned to rustc 1.41.0

---
+++ shell.nix
@@ -0,0 +1,36 @@
+{
+  pkgs ?
+    import (builtins.fetchGit {
+      name = "nixpkgs-with-rustc-1-41-0";
+      url = "https://github.com/NixOS/nixpkgs/";
+      ref = "refs/heads/nixpkgs-unstable";
+      rev = "fcc8660d359d2c582b0b148739a72cec476cfef5";
+    }) {}
+}:
+
+with pkgs;
+mkShell {
+  buildInputs = [
+    alsaLib
+    cmake
+    freetype
+    expat
+    openssl
+    pkgconfig
+    python3
+    rustc
+    vulkan-validation-layers
+    xlibs.libX11
+  ];
+
+  APPEND_LIBRARY_PATH = lib.makeLibraryPath [
+    vulkan-loader
+    xlibs.libXcursor
+    xlibs.libXi
+    xlibs.libXrandr
+  ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$APPEND_LIBRARY_PATH"
+  '';
+}
