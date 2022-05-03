18acf3c0637eb72ed57a39241dca6e04a35973c8
Richard Goulter <richard.goulter@gmail.com>
2022-05-03 21:15:14 +0700

    update shell.nix to use fenix, rust-analyzer

--- shell.nix
+++ shell.nix
@@ -1,35 +1,38 @@
 {
-  pkgs ?
-    import (builtins.fetchGit {
-      name = "nixpkgs-with-rustc-1-41-0";
-      url = "https://github.com/NixOS/nixpkgs/";
-      ref = "refs/heads/nixpkgs-unstable";
-      rev = "fcc8660d359d2c582b0b148739a72cec476cfef5";
-    }) {}
+  pkgs ? import <nixpkgs> {},
+  toolchain ?
+    let
+      fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") { };
+    in
+    fenix.toolchainOf {
+      channel = "1.41.1";
+      sha256 = "sha256-CtlU5P+v0ZJDzYlP4ZLA9Kg5kMEbSHbeYugnhCx0q0Q=";
+    }
 }:
 
-with pkgs;
-mkShell {
-  buildInputs = [
+pkgs.mkShell {
+  buildInputs = with pkgs; [
     alsaLib
     cmake
-    cargo
     freetype
     expat
     openssl
     pkgconfig
     python3
-    rustc
+    rust-analyzer
     vulkan-validation-layers
-    xlibs.libX11
-  ];
+    xorg.libX11
+  ] ++ (with toolchain; [
+    cargo
+    rustc
+  ]);
 
-  APPEND_LIBRARY_PATH = lib.makeLibraryPath [
+  APPEND_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
     vulkan-loader
-    xlibs.libXcursor
-    xlibs.libXi
-    xlibs.libXrandr
-  ];
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXrandr
+  ]);
 
   shellHook = ''
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$APPEND_LIBRARY_PATH"
